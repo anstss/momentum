c45037db3376d639497955a727b2ab04bf5bbb49
anstss <anastasiya31017@gmail.com>
2022-01-28 01:47:33 +0200

    init: add normalize.css

--- momentum/src/index.tsx
+++ momentum/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import "normalize.css";
 
 ReactDOM.render(
   <Provider store={store}>
